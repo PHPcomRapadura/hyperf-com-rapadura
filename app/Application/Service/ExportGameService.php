1f3ffedfcb68c58eb0d57c69458ed390481f0f2d
William Correa <wilcorrea@gmail.com>
2025-03-13 23:01:33 -0300

    feat: vai dar ruim

--- app/Application/Service/ExportGameService.php
+++ app/Application/Service/ExportGameService.php
@@ -6,11 +6,11 @@ namespace App\Application\Service;
 
 use App\Application\Exception\ExportGamesFailedException;
 use App\Domain\Entity\Game;
-use App\Infrastructure\Repository\Postgres\PostgresGameQueryRepository;
+use App\Domain\Repository\GameQueryRepository;
 
 class ExportGameService
 {
-    public function __construct(private readonly PostgresGameQueryRepository $gameQueryRepository)
+    public function __construct(private readonly GameQueryRepository $gameQueryRepository)
     {
     }
 
